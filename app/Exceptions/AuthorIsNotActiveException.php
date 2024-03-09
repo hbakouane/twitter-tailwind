7f3a9df2d38d12539a1c43a85b1171aa13a7e01c
Haytam Bakouane <hbakouane@gmail.com>
2024-03-09 18:46:21 +0100

    Learned more about the tries and exceptions

--- app/Exceptions/AuthorIsNotActiveException.php
+++ app/Exceptions/AuthorIsNotActiveException.php
@@ -2,11 +2,18 @@
 
 namespace App\Exceptions;
 
+use Illuminate\Support\Facades\Log;
+
 class AuthorIsNotActiveException extends \Exception
 {
     public function report()
     {
-        return false;
+        // In this method, we can send emails or reports to an email
+        // stating that an error has been occurred, and we can give more details
+        // or do whatever we want on this method
+        Log::error('This error is coming from an Exception class.', [
+            'exception' => $this
+        ]);
     }
 
     public function render()
